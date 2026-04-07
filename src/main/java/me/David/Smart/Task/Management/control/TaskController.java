504bdcdadef1463d055f2ca73ecccad10a622a80
David <davidtechbuilds@gmail.com>
2026-04-07 14:42:28 +0200

    fix(Restful Mapping)

--- src/main/java/me/David/Smart/Task/Management/control/TaskController.java
+++ src/main/java/me/David/Smart/Task/Management/control/TaskController.java
@@ -3,51 +3,42 @@ package me.David.Smart.Task.Management.control;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import me.David.Smart.Task.Management.model.Status;
-import me.David.Smart.Task.Management.model.Task;
-import me.David.Smart.Task.Management.model.TaskList;
+import me.David.Smart.Task.Management.model.dto.CreateTaskRequest;
 import me.David.Smart.Task.Management.model.dto.TaskDTO;
 import me.David.Smart.Task.Management.model.dto.TaskListDTO;
 import me.David.Smart.Task.Management.model.mapper.TaskMapper;
-import me.David.Smart.Task.Management.repository.TaskRepository;
 import me.David.Smart.Task.Management.service.TaskService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+
 import java.util.UUID;
 
-
 @RestController
-@RequestMapping("api/tasks")
+@RequestMapping("/tasks")
 @AllArgsConstructor
 public class TaskController {
 
     private final TaskService taskService;
     private final TaskMapper taskMapper;
-    //private final UserRepository userRepository;
 
-    @PostMapping("/create")
-    public ResponseEntity<TaskDTO> createTask(@Valid @RequestBody Task task){
-
+    @PostMapping
+    public ResponseEntity<TaskDTO> createTask(@Valid @RequestBody CreateTaskRequest task){
         TaskDTO savedTask = taskService.createTask(task);
-
         return ResponseEntity.status(HttpStatus.CREATED).body(savedTask);
-
     }
 
-    @GetMapping("/getTaskById")
+    @GetMapping("/{id}")
     public ResponseEntity<TaskDTO> getTaskById(@PathVariable UUID id){
-        TaskDTO retrivedTask = taskService.findTaskById(id);
-
-        return ResponseEntity.status(HttpStatus.FOUND).body(retrivedTask);
+        TaskDTO retrievedTask = taskService.findTaskById(id);
+        return ResponseEntity.ok(retrievedTask);
     }
 
-    @GetMapping("/getTaskByStatus")
-    public ResponseEntity<TaskListDTO> getTaskByStatus(@RequestParam Status status){
-        TaskListDTO retrivedTasks = taskService.findTaskByStatus(status);
-
-        return ResponseEntity.status(HttpStatus.FOUND).body(retrivedTasks);
+    @GetMapping
+    public ResponseEntity<TaskListDTO> getTasksByStatus(@RequestParam Status status){
+        TaskListDTO retrievedTasks = taskService.findTaskByStatus(status);
+        return ResponseEntity.ok(retrievedTasks);
     }
 
     @DeleteMapping("/{id}")
